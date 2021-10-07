ce176e9902fc85a7d3ed7a3791030e100573c45d
anirudhkmitl <62011088@kmitl.ac.th>
2021-10-07 19:42:51 +0700

    [ADDED] Login and sign up page

--- main.js
+++ main.js
@@ -18,12 +18,12 @@ $(document).ready(function(){
   
     });
   
-    $('.facility').magnificPopup({
-      delegate:'a',
-      type:'image',
-      gallery:{
-        enabled:true
-      }
-    });
+    // $('.facility').magnificPopup({
+    //   delegate:'a',
+    //   type:'image',
+    //   gallery:{
+    //     enabled:true
+    //   }
+    // });
   
   });
